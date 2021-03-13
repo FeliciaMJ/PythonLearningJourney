1a1318569168695dc7198466154c33675e4d1a36
FeliciaMJ <86763379@qq.com>
2021-03-13 21:22:32 +0800

    [Ticket No:] 20210313 [Description:] 添加有效括号设计程序 [Binary Source:] NA

---
+++ leetcode/20.有效的括号.py
@@ -0,0 +1,24 @@
+def is_valid(s:str) -> bool:
+    """
+    判断一个字符串中的括号是否匹配。
+    :param s:
+    :return:
+    """
+
+    temp = {"{": "}", "[":"]", "(": ")"}
+    stack = []
+
+    for bracket in s:
+        if bracket in temp:
+            stack.append(bracket)
+        else:
+            if stack and bracket == temp.get(stack[-1]):
+                stack.pop()
+            else:
+                return False
+    return not stack
+
+
+if __name__ == '__main__':
+    brackets = "]"
+    print(is_valid(brackets))
