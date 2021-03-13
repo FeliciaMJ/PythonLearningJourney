57cc6cf5d6c77b0f566c6cdabf1edfeea2519ff8
FeliciaMJ <86763379@qq.com>
2021-03-13 22:52:09 +0800

    [Ticket No:] 20210313 [Description:] 添加工厂方法的设计模式 [Binary Source:] NA

---
+++ design_patterns/chapter1/factory_method.py
@@ -0,0 +1,86 @@
+import xml.etree.ElementTree as etree
+import json
+
+from utils.my_logger import logger
+
+"""
+创建型设计模式处理对象创建相关的问题：目标是当直接创建对象不太方便时，提供更好的方式。
+工厂方法：对不同的输入参数返回不同的对象。
+抽象工厂：它是一组用于创建一系列相关事务对象的工厂方法。
+
+"""
+
+
+class JSONConnector(object):
+    def __init__(self, filepath):
+        self.data = dict()
+        with open(filepath, mode='r', encoding='utf-8') as f:
+            self.data = json.load(f)
+
+    @property
+    def parsed_data(self):
+        return self.data
+
+
+class XMLConnector(object):
+    def __init__(self, filepath):
+        self.tree = etree.parse(filepath)
+
+    @property
+    def parsed_data(self):
+        return self.tree
+
+
+def connection_factory(filepath):
+    """
+    对不同的输入参数返回不同的对象。
+    :param filepath:
+    :return:
+    """
+    if filepath.endswith('json'):
+        connector = JSONConnector
+    elif filepath.endswith('xml'):
+        connector = XMLConnector
+    else:
+        raise ValueError('Cannot connect to {}'.format(filepath))
+    return connector(filepath)
+
+
+def connect_to(filepath):
+    """
+    对输入参数进行校验，只有.json和.xml文件的格式才处理，否则会抛出异常。
+    :param filepath:
+    :return:
+    """
+    factory = None
+    try:
+        factory = connection_factory(filepath)
+    except ValueError as ve:
+        logger().info(ve)
+    return factory
+
+
+def main():
+    sqlite_factory = connect_to('data/person.sq3')
+    logger().info(sqlite_factory)
+
+    xml_factory = connect_to('data/person.xml')
+    xml_data = xml_factory.parsed_data
+    liars = xml_data.findall(".//{}[{}='{}']".format('person', 'lastName', 'Liar'))
+    print('found: {} persons'.format(len(liars)))
+    for liar in liars:
+        print('first name: {}'.format(liar.find('firstName').text))
+        print('last name: {}'.format(liar.find('lastName').text))
+        [print('phone number ({})'.format(p.attrib['type']), p.text) for p in liar.find('phoneNumbers')]
+
+    json_factory = connect_to('data/donut.json')
+    json_data = json_factory.parsed_data
+    print('found: {} donuts'.format(len(json_data)))
+    for donut in json_data:
+        print('name: {}'.format(donut['name']))
+        print('price: ${}'.format(donut['ppu']))
+        [print('topping: {} {}'.format(t['id'], t['type'])) for t in donut['topping']]
+
+
+if __name__ == '__main__':
+    main()
