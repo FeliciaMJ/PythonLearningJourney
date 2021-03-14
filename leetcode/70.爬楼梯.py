2e99bea1c2e884148240d75617185ab01247bb46
FeliciaMJ <86763379@qq.com>
2021-03-14 19:33:31 +0800

    [Ticket No:] 20210314 [Description:] 添加LeetCode第70题题解,爬楼梯问题，动态规划 [Binary Source:] NA

---
+++ leetcode/70.爬楼梯.py
@@ -0,0 +1,22 @@
+def climb_stairs(n: int) -> int:
+    """
+    爬楼梯问题
+    :param n:
+    :return:
+    """
+    if n <= 2:
+        return n
+    dp = [0] * n
+    dp[0] = 1
+    dp[1] = 2
+
+    for i in range(2, n):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[-1]
+
+
+if __name__ == '__main__':
+    parameter = 10
+    answer = climb_stairs(parameter)
+    print(answer)
