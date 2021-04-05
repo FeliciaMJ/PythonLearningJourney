0e873b1bef35e393d604ee0a3a0d4cae339b40d4
FeliciaMJ <86763379@qq.com>
2021-04-05 19:12:58 +0800

    [Ticket No:] 20210405 [Description:] 添加Python学习的高级技巧笔记 [Binary Source:] NA

--- nlp_task/text_classification/run_cnn.py
+++ nlp_task/text_classification/run_cnn.py
@@ -5,4 +5,6 @@ import numpy as np
 import tensorflow as tf
 from sklearn import metrics
 from nlp_task.text_classification.cnn_model import TCNNConfig, TextCNN
-from nlp_task.text_classification
+from nlp_task.text_classification.data import cnews_loader
+from nlp_task.text_classification.helper import cnews_group
+
--- practice/demo.py
+++ practice/demo.py
@@ -1,17 +1,72 @@
-import tensorflow as tf
+class MyDataWithMethod(object):
+    def printFoo(self):
+        print("you invoked printFoo()")
 
 
-var1 = tf.Variable(5, name="var1")
-var2 = tf.Variable(6)
-
-var3 = tf.Variable(initial_value=tf.random_uniform(shape=(2, 3), minval=10, maxval=20), name="var3")
-var4 = tf.Variable(tf.truncated_normal(shape=[2, 3], mean=2.0, stddev=0.5, dtype=tf.float32, seed=234), name="var4")
-init = tf.global_variables_initializer()
-with tf.Session() as sess:
-    sess.run(init)
-    print(sess.run(var1))
-    print(sess.run(var1 + var2))
-    print(sess.run(var3))
-    # print(sess.run(var4))
+md = MyDataWithMethod()
+md.printFoo()
 
 
+class AddrBookEntry(object):
+    def __init__(self, nm, ph):
+        self.name = nm
+        self.phone = ph
+        print("Created instance for : ", self.name)
+
+    def update_phone(self, newph):
+        self.phone = newph
+        print("Updated phone # for : ", self.name)
+
+
+ab = AddrBookEntry("iphone11", "123")
+ab.update_phone("234")
+
+
+class EmplAddrBookEntry(AddrBookEntry):
+    def __init__(self, nm, ph, id, em):
+        AddrBookEntry.__init__(self, nm, ph)
+        self.empid = id
+        self.email = em
+
+    def update_email(self, newem):
+        self.email = newem
+        print("Updated e-mail address for: ", self.name)
+
+
+john = EmplAddrBookEntry("John", "408-555-1212", 42, "john@spam.doe")
+print(john.phone)
+print(john)
+print(john.name)
+john.update_phone("123-555-1212")
+print(john.phone)
+
+# 考虑用OOD来工作的一个最重要的原因，在于它直接提供建模和解决现实世界问题和情形的途径。
+# Person类->消费者、技工、出纳员
+# RepairShop 的参与者。
+"""
+抽象：指对现实世界问题和实体的本质表现、行为和特征建模。
+类通常在一个模块的顶层进行定义，以便类实例能够在类定义的源代码文件中的任何地方被创建。
+
+"""
+
+
+class Demo2(object):
+    # my_version = 1.1
+    """
+    这个是类的文档字符串。
+    """
+    def func(self):
+        pass
+
+    def funce(self):
+        raise NotImplementedError("这个方法没有实现")
+
+
+print(Demo2.__dict__)
+print(dir(Demo2))
+print(dir(Demo2) == Demo2.__dict__)
+print(vars(Demo2))
+demo = Demo2()
+print(Demo2.__name__)
+print(Demo2.__doc__)
+print(Demo2.__module__)
